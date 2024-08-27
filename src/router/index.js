a52c916add5a311484e457ce4b86983e06080e37
ZhangZJ0906 <cklgy88@gmail.com>
2024-08-27 23:35:05 +0800

    老會踩雷了

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,41 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/signup',
-      name: 'signup',
-      component: () =>import  ('../views/signupView.vue')
+      path: "/signup",
+      name: "signup",
+      component: () => import("../views/signupView.vue"),
     },
     {
-      path: '/todolist',
-      name: 'todolist',
-      component: () =>import  ('../views/TodoListView.vue')
+      path: "/todolist",
+      name: "todolist",
+      component: () => import("../views/TodoListView.vue"),
     },
-    
-  ]
-})
+  ],
+});
+router.beforeEach((to, from, next) => {
+  // 检查目标路由是否需要 token
+  if (to.name === "todolist") {
+    const token = sessionStorage.getItem("token"); // 从 sessionStorage 获取 token
+    if (token) {
+      // 如果有 token，则继续导航
+      next();
+    } else {
+      // 如果没有 token，则重定向到主页或登录页面
+      next({ name: "home" });
+    }
+  } else {
+    // 对其他路由不做拦截，直接继续导航
+    next();
+  }
+});
 
-export default router
+export default router;
